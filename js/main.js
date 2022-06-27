d0028a7cfd4b88c881f27d4c96517356bb0c3ab9
Lili Fernandez <lilibetfdez@gmail.com>
2022-06-27 11:30:37 +0200

    add variable y html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,62 @@
 "use strict";
 
+const kittenImageOne = `https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg`;
+const kittenNameOne = `Anastacio`;
+const kittenDescOne = `Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+moleste. Es una maravilla acariciarle!`;
+const kittenRaceOne = `British Shorthair`;
+
+const kittenImageTwo = `https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg`;
+const kittenNameTwo = `Fiona`;
+const kittenDescTwo = `Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+moleste. Es una maravilla acariciarle!`;
+const kittenRaceTwo = `British Shorthair`;
+
+const kittenImageThree = `https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg`;
+const kittenNameThree = `Cielo`;
+const kittenDescThree = `Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+moleste. Es una maravilla acariciarle!`;
+const kittenRaceThree = `British Shorthair`;
 
 const newForm= document.querySelector(".js-new-form");
 newForm.classList.remove("collapsed");
 
+const dataList = document.querySelector(".js-list");
+const kittenOne = `<li class="card">
+<article>
+  <img class="card_img" src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg" alt="gatito" />
+  <h3 class="card_title">kittenNameOne</h3>
+  <h4 class="card_race">British Shorthair</h4>
+  <p class="card_description">
+    Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+    moleste. Es una maravilla acariciarle!
+  </p>
+</article>
+</li>`;
+
+
+const kittenTwo = `<li class="card">
+<img class="card_img"
+  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />
+<h3 class="card_title">kittenNameTwo</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
+
+
+const kittenTrhee = `<li class="card">
+<img class="card_img"
+  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
+  alt="gatito" />
+<h3 class="card_title">kittenNameThree</h3>
+<h4 class="card_race">British Shorthair</h4>
+<p class="card_description">
+  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
+  Es una maravilla acariciarle!
+</p>
+</li>`;
+
+dataList.innerHTML = kittenOne+kittenTwo+kittenTrhee
